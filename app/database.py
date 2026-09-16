f2f30f5cee80a57d237bef1bac414dd02629fed1
Dauwn9 <pshtaevdauren96@gmail.com>
2026-09-16 23:04:30 +0500

    Created database by init_db.py file. Added engine and sessionmaker in database.py

--- app/database.py
+++ app/database.py
@@ -1,4 +1,9 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine
 
 class Base(DeclarativeBase):
     pass
+
+engine = create_engine("sqlite:///finance_tracker.db", echo=True)
+
+Session = sessionmaker(engine)
